3d8573c048379ec416386782bf7781d3c2477843
Agbaje Franklin <franchoal1919@gmail.com>
2026-09-25 06:51:20 +0100

    Register users and profiles in admin

--- agrichub_backend/accounts/admin.py
+++ agrichub_backend/accounts/admin.py
@@ -1,3 +1,106 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User, Profile
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+        "date_joined",
+    )
+
+    list_filter = (
+        "is_staff",
+        "is_active",
+        "is_verified",
+    )
+
+    ordering = ("-date_joined",)
+
+    search_fields = (
+        "email",
+        "first_name",
+        "last_name",
+        "phone_number",
+    )
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Personal information",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "phone_number",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "is_verified",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Important dates",
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                    "created_at",
+                    "updated_at",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "location",
+        "created_at",
+        "updated_at",
+    )
+
+    search_fields = (
+        "user__email",
+        "user__first_name",
+        "user__last_name",
+        "location",
+    )
